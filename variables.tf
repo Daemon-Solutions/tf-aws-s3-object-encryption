d80a7844d5f593d62897df0f5ce796ff3e4fbe4f
john.george <john.george@claranet.uk>
2017-09-26 15:40:45 +0100

    Tidied up and added description/type to all variables

--- variables.tf
+++ variables.tf
@@ -1,14 +1,30 @@
-
-variable "encrypted_bucket_name" {}
-
-variable "kms_key_description" {
-  default = ""
+/* S3 bucket variables */
+variable "encrypted_bucket_name" {
+  description = "The name of the s3 bucket to create"
+  type = "string"
+  default = "encrypted-bucket"
 }
 
-variable "encrypted_file_path" {}
-
-variable "encrypted_object" {}
-
 variable "bucket_versioning" {
+  description = "Whether to enable s3 bucket versioning"
+  type = "string"
   default = false
+}
+
+/* KMS key variables */
+variable "kms_key_description" {
+  description = "The description for the KMS key you wish to create"
+  type = "string"
+  default = "This KMS key is for encrypting an item in an s3 bucket"
+}
+
+/* Encrypted file variables */
+variable "encrypted_file_path" {
+  description = "The local path for the file you wish to upload and encrypt"
+  type = "string"
+}
+
+variable "encrypted_object" {
+  description = "The name of the file you wish to encrypt"
+  type = "string"
 }
